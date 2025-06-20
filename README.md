698af0e2a48fe964261425f49a22450aa07048ee
Lavanya Anand <lavanyaanand0311@gmail.com>
2025-06-20 18:32:31 +0530

    Model evaluation and MLFlow added

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # End-to-End-Project-ML-Flow
+
+# Developement steps
+
+1. Update config.yaml - raw details
+2. Update schema.yaml - Dependent and independent columns
+3. Update params.yaml - Model parameters
+4. Update the entity - Configuration classes
+5. Update the Configuration manager in src config - Creating directories and calling class, Assigning the raw details to class variables
+6. Update the components - separate functions for each configuration manager
+7. Update the pipeline - Calling those componnets classes and creating a pipeline
+8. Update the main.py - Triggering those pipelines
+9. Update the app.py
+
+# Steps to Run
+
+1. Clone the repository
+2. Create a conda environment:
+    conda create -n mlproj python=3.8 -y
+    conda activate mlproj
+3. Install Requirements
+    pip install -r requiremnts.txt
+4. python app.py
+
+# MLFlow - DagsHub
+
+1. Integrate the github repository in DagsHub
+2. Copy the URI, username and password from Remote -> Experiment
+3. Modify the following code to declare it as env variable 
+``` bash
+export MLFLOW_TRACKING_URI=
+export MLFLOW_TRACKING_USERNAME=
+export MLFLOW_TRACKING_PASSWORD=
+```
+
+
+
+
--- src/mlProject/components/model_evaluation.py
+++ src/mlProject/components/model_evaluation.py
@@ -7,6 +7,8 @@ import joblib
 from mlProject.utils.common import save_json
 from mlProject.entity.config_entity import ModelEvaluationConfig
 from pathlib import Path
+from mlProject.utils.common import save_json
+import mlflow
 
 class ModelEvaluation:
     def __init__(self, config: ModelEvaluationConfig):
@@ -21,7 +23,7 @@ class ModelEvaluation:
     
 
 
-    def save_results(self):
+    def log_into_mlflow(self):
 
         test_data = pd.read_csv(self.config.test_data_path)
         model = joblib.load(self.config.model_path)
@@ -29,12 +31,23 @@ class ModelEvaluation:
         test_x = test_data.drop([self.config.target_column], axis=1)
         test_y = test_data[[self.config.target_column]]
         
-        predicted_qualities = model.predict(test_x)
+        mlflow.set_registry_uri(self.config.mlflow_uri)
+        tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
 
-        (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
-        
-        # Saving metrics as local
-        scores = {"rmse": rmse, "mae": mae, "r2": r2}
-        save_json(path=Path(self.config.metric_file_name), data=scores)
+        with mlflow.start_run():
+            predicted_qualities=model.predict(test_x)
+            (rmse,mae,r2) = self.eval_metrics(test_y, predicted_qualities)
 
+            scores={'rmse':rmse, 'mae':mae,'r2':r2}
+            save_json(path=Path(self.config.metric_file_name), data=scores)
+
+            mlflow.log_params(self.config.all_params)
+            mlflow.log_metric('rmse',rmse)
+            mlflow.log_metric('r2',r2)
+            mlflow.log_metric('mae',mae)
+            
+            if tracking_url_type_store!='file':
+                mlflow.sklearn.log_model(model,"model",registered_model_name='ElasticnetModel')
+            else:
+                mlflow.sklearn.log_model(model,"model")
 
